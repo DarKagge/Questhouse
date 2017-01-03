2af763a1a0fafc02f60e29e66b87cfdfa3408e42
jeka <ElectroDeggi33759@yandex.ru>
2017-01-03 16:57:41 +0200

    Old

--- app/js/common.js
+++ app/js/common.js
@@ -133,4 +133,47 @@ $(function() {
 	$('#total').text(autoplaySlider.getTotalSlideCount());
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+		/* This is basic - uses default settings */
+
+		$("a#single_image").fancybox();
+
+		/* Using custom settings */
+
+		$("a#inline").fancybox({
+			'hideOnContentClick': true
+		});
+
+		/* Apply fancybox to multiple items */
+
+		$("a.group").fancybox({
+			'transitionIn'	:	'elastic',
+			'transitionOut'	:	'elastic',
+			'speedIn'		:	600,
+			'speedOut'		:	200,
+			'overlayShow'	:	false
+		});
+
+
+
+
+
+
+
+
+
+
 });
